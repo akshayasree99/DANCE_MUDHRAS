4fe64ccd4d0b37809e1aa56c4e3bd6495c3a7aba
progjuggler <progjuggler@gmail.com>
2024-08-17 23:25:05 +0530

    <cover-page>

--- m.js
+++ m.js
@@ -2,3 +2,7 @@ const click=document.querySelector(".enter button");
 click.addEventListener("click",()=>{
     window.location.href ='index.html'
 })
+const btn=document.querySelector(".enter-button button");
+        btn.addEventListener("click",()=>{
+            window.open("https://dance-mudhras.vercel.app/");
+        })
